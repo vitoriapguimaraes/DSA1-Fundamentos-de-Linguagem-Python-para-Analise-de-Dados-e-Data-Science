ffbcc2e7a67fdbd86ae7950cf9f6bf2d5334eb40
Vitória Guimarães <vpistoriguimaraes@gmail.com>
2024-09-28 19:39:03 -0300

    cap11 in

--- Cap10/part6.py
+++ Cap10/part6.py
@@ -30,8 +30,42 @@ df['species'] = data['target']
 print(df.head())
 
 # Para criar um gráfico de linhas com todas as variáveis do dataframe, basta fazer isso:
-# df.plot()
+# figura 1
+df.plot()
+
+# Que tal um scatter plot com duas variáveis?
+# figura 2
+df.plot.scatter(x = 'sepal length (cm)', y = 'sepal width (cm)')
+
+# E mesmo gráficos mais complexos, como um gráfico de área, pode ser criado:
+# figura 3
+columns = ['sepal length (cm)', 'petal length (cm)', 'petal width (cm)', 'sepal width (cm)']
+df[columns].plot.area()
+
+# Calculamos a média das colunas agrupando pela coluna species e criamos um gráfico de barras com o resultado
+# figura 4
+df.groupby('species').mean().plot.bar()
+
+# Ou então, fazemos a contagem de classes da coluna species e plotamos em um gráfico de pizza
+# figura 5
+df.groupby('species').count().plot.pie(y = 'sepal length (cm)')
+
+
+# A lista de possibilidades é imensa. Aqui tem muitos outros exemplos:
+# https://pandas.pydata.org/pandas-docs/stable/user_guide/visualization.html
+
+# Gráfico KDE (Kernel Density Function) para cada variável do dataframe
+# figura 6
+df.plot.kde(subplots = True, figsize = (8,8))
+
+# Boxplot de cada variável numérica
+# figura 7
+columns = ['sepal length (cm)', 'petal length (cm)', 'petal width (cm)', 'sepal width (cm)']
+df[columns].plot.box(figsize = (8,8))
+
+
 plt.show()
 
+print("Os gráficos foram gerados")
 
 print("-"*50)
--- template-part.py
+++ template-part.py
@@ -1,6 +1,6 @@
-# CURSO: Fundamentos de Linguagem Python para Analise de Dados e Data Science == CAPÍTULO 10
+# CURSO: Fundamentos de Linguagem Python para Analise de Dados e Data Science == CAPÍTULO 11
 
-### PANDAS ###
+### Análise e Ciência de Dados via programação Python através de duas poderosas bibliotecas: Matplotlib e Seaborn ###
 
 ### aaaaa ###
 
