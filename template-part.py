4e018febde54d662f112c9581d14edeb2e1db666
Vitória Guimarães <vpistoriguimaraes@gmail.com>
2024-10-02 15:17:16 -0300

    cap14 in

--- template-part.py
+++ template-part.py
@@ -1,6 +1,6 @@
-# CURSO: Fundamentos de Linguagem Python para Analise de Dados e Data Science == CAPÍTULO 12
+# CURSO: Fundamentos de Linguagem Python para Analise de Dados e Data Science == CAPÍTULO 14
 
-### Python e Linguagem SQL ###
+### Análise estatística com o pacote Statsmodels em Linguagem Python ###
 
 ### aaaaa ###
 
