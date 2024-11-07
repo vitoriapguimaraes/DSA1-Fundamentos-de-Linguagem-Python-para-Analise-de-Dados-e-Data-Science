26527a5cd7f9c538749d90484dd7b078764d5ddf
Vitória Guimarães <87993956+vitoriapguimaraes@users.noreply.github.com>
2024-11-07 16:38:23 -0300

    Delete Cap19-projeto directory

--- Cap19-projeto/main.py
+++
@@ -1,5 +0,0 @@
-# CURSO: Fundamentos de Linguagem Python para Analise de Dados e Data Science
-# => CAPÍTULO 19
-
-# Seja bem-vindo(a) ao Capítulo 19. Este capítulo traz para você o terceiro e
-# último projeto do curso, com a construção de um chatbot
